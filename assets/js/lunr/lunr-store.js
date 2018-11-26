da5f2742465f588f5cb096125fbeda3d6ad443b6
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2018-11-26 12:42:51 +0000

    Update docs after building Travis build 88 of AnyBody/anyscript
    The docs were built from the branch 'master' against the commit
    9f00995996c0d2f7a83955c70a90194db5626750.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/AnyBody/anyscript/jobs/459727426.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/deploy-environment/bin/doctr deploy --built-docs ./_site --deploy-branch-name master --deploy-repo AnyBody/anybody.github.io .

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Our new AnyScript landing page",
-        "excerpt":"We are building a replacement for the old AnyScript.org page. The site is currently not active but the work is ongoing.                                                                     Old and new AnyScript.org site  The new site links to the new version of the repository site and the wiki site, which are both hosted on GitHub. ","categories": ["News"],
+        "excerpt":"We are building a replacement for the old AnyScript.org page. The site is currently not active but the work is ongoing.                                                                                      Old and new AnyScript.org site       The new site links to the new version of the repository site and the wiki site, which are both hosted on GitHub.  ","categories": ["News"],
         "tags": ["web site"],
         "url": "https://anyscript.org/news/new-anyscript-site/",
         "teaser":"https://anyscript.org/assets/images/teaser.jpg"},{
@@ -10,47 +10,47 @@ var store = [{
         "url": "https://anyscript.org/tips-n-tricks/useful-anyscript-macros/",
         "teaser":"https://anyscript.org/assets/images/macro_post_teaser.png"},{
         "title": "Creating videos",
-        "excerpt":"In this post, I will show a new AnyScript template to create awesome videos of yourmusculoskeletal simulations with a single click. Download on GitHub Notice: You can now find the template directly in the AMMR. Include it like this: #include &quot;&lt;ANYBODY_PATH_MODELUTILS&gt;/Video/CameraClassTemplate.any&quot;Previously, I often spent hours creating videos for presentations, etc.....","categories": ["Tips-n-Tricks"],
+        "excerpt":"In this post, I will show a new AnyScript template to create awesome videos of your musculoskeletal simulations with a single click. Download on GitHub Notice: You can now find the template directly in the AMMR. Include it like this: #include &quot;&lt;ANYBODY_PATH_MODELUTILS&gt;/Video/CameraClassTemplate.any&quot; Previously, I often spent hours creating videos for...","categories": ["Tips-n-Tricks"],
         "tags": ["Creating Videos"],
         "url": "https://anyscript.org/tips-n-tricks/creating-videos-from-your-simulations/",
         "teaser":"https://anyscript.org/assets/images/teaser.jpg"},{
         "title": "Create Animated GIFs",
-        "excerpt":"If a picture is worth a thousand words. What is the value of an animated GIF? If anyone knew the answer to this question it was Eadweard Muybridge who was an 19th century photographer and the grandfather of today’sbiomechanical scientists. He used sequences of photographs to analyze human andanimal motion....","categories": ["Tips-n-Tricks"],
+        "excerpt":"If a picture is worth a thousand words. What is the value of an animated GIF? If anyone knew the answer to this question it was Eadweard Muybridge who was an 19th century photographer and the grandfather of today’s biomechanical scientists. He used sequences of photographs to analyze human and...","categories": ["Tips-n-Tricks"],
         "tags": ["Creating Videos"],
         "url": "https://anyscript.org/tips-n-tricks/create-animated-gifs/",
         "teaser":"https://anyscript.org/assets/images/posts/gif-teaser.gif"},{
         "title": "Notepad++ and AnyScript files",
-        "excerpt":"In this post, I will show an extension to get syntaxhighlighting and code folding working forNotepad++. This the first post in a series on using external editors with the AnyBodyModeling System. Why another editor? I shall be the first to admit that the editor in the AnyBody Modeling System(AMS) can...","categories": ["Editors"],
+        "excerpt":"In this post, I will show an extension to get syntax highlighting and code folding working for Notepad++. This the first post in a series on using external editors with the AnyBody Modeling System. Why another editor? I shall be the first to admit that the editor in the AnyBody...","categories": ["Editors"],
         "tags": ["Syntax highlighting"],
         "url": "https://anyscript.org/editors/notepad++-and-anyscript/",
         "teaser":"https://anyscript.org/assets/images/posts/npp-teaser.png"},{
         "title": "AnyMoCap: A model framework motion caputure based models",
-        "excerpt":"The AnyMoCap model is an effort to create a simple and unified framework fordoing any kind of mocap analysis with the AnyBody ModelingSystem. This is first post in a series on the AnyMoCap framework. In a series posts Iwill show how to use the frame and presents some of the...","categories": ["AnyMoCap"],
+        "excerpt":"The AnyMoCap model is an effort to create a simple and unified framework for doing any kind of mocap analysis with the AnyBody Modeling System. This is first post in a series on the AnyMoCap framework. In a series posts I will show how to use the frame and presents...","categories": ["AnyMoCap"],
         "tags": ["Motion Capture"],
         "url": "https://anyscript.org/anymocap/anymocap-motion-capture-framework/",
         "teaser":"https://anyscript.org/assets/images/posts/anymocap_teaser.png"},{
         "title": "Calculating the Instantaneous Axis of Rotation",
-        "excerpt":"The instantaneous axis of rotation between two bodies is a useful concept inbiomechanics. In this post, we will dig into how to calculate the instantaneousaxis of rotation and show an AnyScript class_template that calculate and displays theaxis between any two reference frames in the AnyBodyModeling System. Before we dive into...","categories": ["Tools"],
+        "excerpt":"The instantaneous axis of rotation between two bodies is a useful concept in biomechanics. In this post, we will dig into how to calculate the instantaneous axis of rotation and show an AnyScript class_template that calculate and displays the axis between any two reference frames in the AnyBody Modeling System....","categories": ["Tools"],
         "tags": ["Kinematics"],
         "url": "https://anyscript.org/tools/instantaneous-axis-of-rotation/",
         "teaser":"https://anyscript.org/assets/images/posts/iaor_teaser.png"},{
         "title": "Add limits to the Range of Motion",
-        "excerpt":"In this post, I will show a new AnyScript class template to add range-of-motionlimits to Human body model. The implementation consist of two AnyScript class templates. A highlevel class template RangeOfMotionLimits which makes it easy to addRange-of-motion limits to all the joints on the Musculoskeltal model. Behind the scenes the...","categories": ["tools"],
+        "excerpt":"In this post, I will show a new AnyScript class template to add range-of-motion limits to Human body model. The implementation consist of two AnyScript class templates. A high level class template RangeOfMotionLimits which makes it easy to add Range-of-motion limits to all the joints on the Musculoskeltal model. Behind...","categories": ["tools"],
         "tags": ["kinematics","joints"],
         "url": "https://anyscript.org/tools/rom-limits/",
         "teaser":"https://anyscript.org/assets/images/posts/rom-limits_teaser.png"},{
         "title": "AnyScript support in Visual Studio Code",
-        "excerpt":"Visual Studio Code(VS Code) is one of the newest texteditors to join the group of new powerful source code editors like Atom, Sublime Text and brackets. All of which have become extremely popular in recent years. Visual Studio Code is currently my favorite text editor. It is cross-platform, lightweight, extensible...","categories": ["Editors"],
+        "excerpt":"Visual Studio Code(VS Code) is one of the newest text editors to join the group of new powerful source code editors like Atom, Sublime Text and brackets. All of which have become extremely popular in recent years. Visual Studio Code is currently my favorite text editor. It is cross-platform, lightweight,...","categories": ["Editors"],
         "tags": ["Syntax highlighting","Visual Studio Code"],
         "url": "https://anyscript.org/editors/anyscript-in-vscode/",
         "teaser":"https://anyscript.org/assets/images/posts/vscode-teaser.png"},{
         "title": "Freeing the AnyBody Tutorials",
-        "excerpt":"Anyone, who works with AnyBody modeling system has at one time used the AnyBodytutorials. They are a valuable resource when learning to use the AnyBodyModeling System and there are more than 15 different tutorials coveringeverything from ‘Getting Started’ tutorials for the new users to very advancedtopics like force dependent kinematics...","categories": ["news"],
+        "excerpt":"Anyone, who works with AnyBody modeling system has at one time used the AnyBody tutorials. They are a valuable resource when learning to use the AnyBody Modeling System and there are more than 15 different tutorials covering everything from ‘Getting Started’ tutorials for the new users to very advanced topics...","categories": ["news"],
         "tags": ["Tutorials","Sphinx"],
         "url": "https://anyscript.org/news/freeing-the-anybody-tutorials/",
         "teaser":"https://anyscript.org/assets/images/posts/tutorials_teaser.png"},{
         "title": "Getting AnyBody and Numpy to work",
-        "excerpt":"With the release of the AnyBody Modeling System 7.1 update, we thought it wastime to make a post about all the improvements in the new model repository. The AnyBody Managed Model Repository (AMMR) version 2.0 marks the culmination ofa huge task to update our existing models and adding new models...","categories": ["news"],
+        "excerpt":"With the release of the AnyBody Modeling System 7.1 update, we thought it was time to make a post about all the improvements in the new model repository. The AnyBody Managed Model Repository (AMMR) version 2.0 marks the culmination of a huge task to update our existing models and adding...","categories": ["news"],
         "tags": ["AMMR","Models"],
         "url": "https://anyscript.org/news/Numpy-and-scipy/",
         "teaser":"https://anyscript.org/assets/images/posts/ammr2_teaser.png"},{
@@ -60,22 +60,22 @@ var store = [{
         "url": "https://anyscript.org/news/ammr-2.0/",
         "teaser":"https://anyscript.org/assets/images/posts/ammr2_teaser.png"},{
         "title": "AMMR now has a DOI",
-        "excerpt":"Digital Object Identifiers (DOIs) arethe foundation of references and metrics in the world of academia. With the releaseof AMMR 2.1.1 we have also added a DOI to the AnyBody Managed Model Repository (AMMR): . The DOI for the AMMR are issues by Zenodo, which isan European Open Science platform hosted...","categories": ["news"],
+        "excerpt":"Digital Object Identifiers (DOIs) are the foundation of references and metrics in the world of academia. With the release of AMMR 2.1.1 we have also added a DOI to the AnyBody Managed Model Repository (AMMR): . The DOI for the AMMR are issues by Zenodo, which is an European Open...","categories": ["news"],
         "tags": ["AMMR","Models"],
         "url": "https://anyscript.org/news/DOI/",
         "teaser":"https://anyscript.org/assets/images/posts/doi_teaser.png"},{
         "title": "Body Model Configuration App",
-        "excerpt":"One of the first things I did when starting to use the AnyBody ModelingSystem, was to understand how to configure the human model. This means enabling and disabling different body parts and setting up the available options for muscles, ligamentsetc. The model is configured via AnyScript by defining switcheswhich we...","categories": ["Tools"],
+        "excerpt":"One of the first things I did when starting to use the AnyBody Modeling System, was to understand how to configure the human model. This means enabling and disabling different body parts and setting up the available options for muscles, ligaments etc. The model is configured via AnyScript by defining...","categories": ["Tools"],
         "tags": ["Model configuration","BM switches"],
         "url": "https://anyscript.org/tools/body-model-configurator/",
         "teaser":"https://anyscript.org/assets/images/posts/BM_Plugin_teaser.JPG"},{
         "title": "Calculating maximum model strength",
-        "excerpt":"When working with subject specific scaling of models, it can be avaluable tool to know what the strength of your model is for a givenposture. With strength we mean the maximum permissible load that the model can carry in a given posture.This post will show you a way of calculating...","categories": ["tools"],
+        "excerpt":"When working with subject specific scaling of models, it can be a valuable tool to know what the strength of your model is for a given posture. With strength we mean the maximum permissible load that the model can carry in a given posture. This post will show you a...","categories": ["tools"],
         "tags": ["strength","muscle"],
         "url": "https://anyscript.org/tools/estimating-maximum-model-strength/",
         "teaser":"https://anyscript.org/assets/images/posts/max-strength_teaser.png"},{
         "title": "Solving optimization problems using Python",
-        "excerpt":"The AnyBody Modeling System (AMS) provides a build-in optimizationclass AnyOptStudy, and with it you have the opportunity to solve advanced mathematical optimization problems. You can get a taste of how it works in this new tutorialOf course there can be situations were you want to do a little more than...","categories": ["tools"],
+        "excerpt":"The AnyBody Modeling System (AMS) provides a build-in optimization class AnyOptStudy, and with it you have the opportunity to solve advanced mathematical optimization problems. .. seealso:: You can get a taste of how it works in the newly updated tutorial on parameter and optimization studies Of course there can be...","categories": ["tools"],
         "tags": ["python","Optimization"],
         "url": "https://anyscript.org/tools/Solving-Optimization-Problems-Using-Python/",
         "teaser":"https://anyscript.org/assets/images/teaser.jpg"}]
